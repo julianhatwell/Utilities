d991cfd2f211c07f3d7f4c70e4bc9821f93c9f98
julianhatwell <julian.hatwell@googlemail.com>
2016-08-16 00:22:06 +0800

    fix

---
+++ Utilities.R
@@ -0,0 +1,91 @@
+# paste two strings together - for creating file names from params
+me_File <- function(f) { paste0(f, "_File") }
+
+# paste two strings together - for combining file paths
+pathTo <- function(f) { paste0(fileLoc, f) }
+
+# deletes a file
+fileInit <- function(f) { if (file.exists(f)) file.remove(f) }
+
+save.as.csv <- function(df, fName) {
+  write.csv(df, pathTo(paste0(fName, ".csv")))
+} 
+
+# give it a vector and returns a function to search that vector
+find_in_vector <- function(vec) { function(find) { any(find == vec) } }
+
+# prediction function for regsubsets objects
+predict.regsubsets <- function(object, newdata, id, ...) {
+  form <- as.formula(object$call[[2]])
+  mat <- model.matrix(form,newdata)
+  coefi <- coef(object, id = id)
+  mat[,names(coefi)]%*%coefi
+}
+
+<<<<<<< HEAD
+# metrics from confusion matrix
+createMetrics <- function(confmat) {
+  metrics <- if (dim(confmat)[1] == 2) {
+    ntn <- confmat[1,1]
+    nfn <- confmat[1,2]
+    ntp <- confmat[2,2]
+    nfp <- confmat[2,1]
+    list(
+      confmat = confmat
+      , ntn = ntn
+      , nfn = nfn
+      , ntp = ntp
+      , nfp = nfp
+      , accuracy = (ntn+ntp)/sum(confmat)
+      , npv = ntn/(ntn+nfn) # accurate negative predictions
+      , ppv = ntp/(nfp+ntp) # precision, accurate positive predictions
+      , specificity = ntn/(ntn+nfp) # negative cases found
+      , sensitivity = ntp/(nfn+ntp) # recall, positive cases found
+      , fpr = nfp/(nfp+ntn) # false positive rate, fp out of all negative predictions
+    )
+  } else {
+    if (which(rownames(confmat) == colnames(confmat)) == 1) {
+      ntn <- confmat[1,1]
+      nfn <- confmat[1,2]
+      ntp <- 0
+      nfp <- 0
+      
+      npv <- 1
+      ppv <- 0
+      specificity <- 1
+      sensitivity <- 0
+      fpr <- 0
+    } else {
+      ntn <- 0
+      nfn <- 0
+      ntp <- confmat[2,2]
+      nfp <- confmat[2,1]
+      
+      npv <- 0
+      ppv <- 1
+      specificity <- 0
+      sensitivity <- 1
+      fpr <- 1
+    }
+    list(
+      confmat = confmat
+      , ntn = ntn
+      , nfn = nfn
+      , ntp = ntp
+      , nfp = nfp
+      , accuracy = accuracy <- confmat[rownames(confmat) == colnames(confmat)]/sum(confmat)
+      , npv = npv
+      , ppv = ppv
+      , specificity = specificity
+      , sensitivity = sensitivity
+      , fpr = fpr
+    )
+  }
+  return(metrics)
+  =======
+    # return only the positive part or zero of a numeric vector
+    pos.part <- function(x) {
+      x <- ifelse(x < 0, 0, x)
+      x
+      >>>>>>> 450ca409aa03e994ac0f1fce4579083d26e831a4
+    }
